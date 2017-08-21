40743ddd510bc9e9418d33cf204248568741f799
iibi0107 <iibi0107@iibi0107-PC>
2017-08-21 11:08:25 +0900

    MyController추가

--- TestGitWeb/src/main/java/my/com/MyController.java
+++ TestGitWeb/src/main/java/my/com/MyController.java
@@ -1,10 +1,11 @@
 package my.com;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MyController {
-
+	@RequestMapping("/testGit")
 	public String testGit(){
 		return "Hello";
 	}
